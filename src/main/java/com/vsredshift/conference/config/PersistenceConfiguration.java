baa7abdabb9611e64b13c3f9cc6b5af4c789647d
Martin Oshaughnessy <vsredshift@gmail.com>
2021-06-04 14:34:23 +0200

    Remove hard-coded db url

--- src/main/java/com/vsredshift/conference/config/PersistenceConfiguration.java
+++ src/main/java/com/vsredshift/conference/config/PersistenceConfiguration.java
@@ -9,7 +9,7 @@ import javax.sql.DataSource;
 @Configuration
 public class PersistenceConfiguration {
 
-    @Bean
+/*    @Bean
     public DataSource dataSource() {
         DataSourceBuilder builder = DataSourceBuilder.create();
         builder.url("jdbc:postgresql://localhost:5432/conference_app");
@@ -17,5 +17,5 @@ public class PersistenceConfiguration {
         builder.password("Welcome");
         System.out.println("Custom data source has been initialised and set");
         return builder.build();
-    }
+    }*/
 }
